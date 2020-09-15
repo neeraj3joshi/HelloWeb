d2ec63b6800bb8bc5597eaef8760658e1f9f2db7
Joshi <neeraj.joshi@kroger.com>
2020-09-15 05:59:31 -0400

    changes done for dispatcher servlet settings

--- src/main/java/com/devops/spring/HelloController.java
+++ src/main/java/com/devops/spring/HelloController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class HelloController {
 
 	@RequestMapping(method = RequestMethod.GET)public String printHello(ModelMap model) {
-	      model.addAttribute("message", "Hello Spring MVC Framework!");
+	      model.addAttribute("message", "Hello Spring MVC Framework Poll!");
 	      return "hello";
 	   }
 }
