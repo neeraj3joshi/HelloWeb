4772167043ac780a953f3cceaff1370d7357fa94
Joshi <neeraj.joshi@kroger.com>
2020-09-16 11:45:12 -0400

    changes done for ansible

--- src/main/java/com/devops/spring/HelloController.java
+++ src/main/java/com/devops/spring/HelloController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class HelloController {
 
 	@RequestMapping(method = RequestMethod.GET)public String printHello(ModelMap model) {
-	      model.addAttribute("message", "Hello Spring MVC Framework Poll Docker!");
+	      model.addAttribute("message", "Hello Spring MVC Framework Poll Docker Ansible!");
 	      return "hello";
 	   }
 }
